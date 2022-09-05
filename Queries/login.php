a0e0e80df1cd05d76defaad83cc4665d855d51d0
Yash Mandaliya <112624754+yashmandaliya@users.noreply.github.com>
2022-09-05 17:02:59 +0530

    Create login.php

---
+++ Queries/login.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+<title>LOGIN</title>
+</head>  
+<body>
+<form action="Insert.php" method="post">
+<table border="2px">
+<tr>
+   <th>Username:</th>
+   <td><input type="text" name="username" id="username" placeholder="Enter Username"></td>
+</tr>
+<tr>
+   <th>Password:</th>
+   <td><input type="password" name="password" id="password" placeholder="Enter Username"></td>
+</tr>
+<tr>
+	<td></td>
+	<td><input type="submit" name="submit"></td>
+</tr>
+</table>    
+</form>
+</body>
+</html>
